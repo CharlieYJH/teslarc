34c2101fb89bc3258d370a9aeecabfe169974970
Charlie Yin <cyin@fortinet.com>
2019-08-28 18:06:02 -0700

    Add binary install target

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@
 #===============================================================================
 function usage ()
 {
-    echo "Usage :  $0 [options] [--]
-
-    Options:
-    -h|help       Display this message
-    -d|debug      Build with debug enabled"
+    echo "Usage :  $0 [options] [--]"
+    echo ""
+    echo "Options:"
+    echo "  -h|help       Display this message"
+    echo "  -d|debug      Build with debug enabled"
+    echo "  -i|install    Install binary after building"
 
 }    # ----------  end of function usage  ----------
 
@@ -18,24 +19,27 @@ function usage ()
 #  Handle command line arguments
 #-----------------------------------------------------------------------
 
-while getopts ":hd" opt
-do
-    case $opt in
-        h|help )
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -h|help )
             usage;
-            exit 0
-            ;;
-        d|debug )
-            CMAKE_BUILD_FLAG="-DCMAKE_BUILD_TYPE=Debug"
-            ;;
+            exit 0;;
+        -d|debug )
+            CMAKE_BUILD_FLAG="-DCMAKE_BUILD_TYPE=Debug";
+            shift;;
+        -i|install )
+            INSTALL_BIN=1;
+            shift;;
+        -* )
+            echo -e "\nOption does not exist: $1\n";
+            usage;
+            exit 1;;
         * )
-            echo -e "\n  Option does not exist : $OPTARG\n"
+            echo -e "\nOption does not exist: $1\n";
             usage;
-            exit 1
-            ;;
+            exit 1;;
     esac
 done
-shift $(($OPTIND-1))
 
 #-----------------------------------------------------------------------
 # Build binary
@@ -48,3 +52,14 @@ if [ ! -d "$BUILD_DIR" ]; then
 fi
 
 cd "$BUILD_DIR" && cmake $CMAKE_BUILD_FLAG .. && make
+
+if [ ! -z "$INSTALL_BIN" ]; then
+    if [ ! "$(id -u)" -eq  0 ]; then
+        echo "You must be root to install this binary!"
+        exit 1
+    fi
+
+    make install
+fi
+
+exit 0
